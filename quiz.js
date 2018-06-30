fa12f676b62836affa436f7e35afbabf50d33494
defalt42 <bgilbert1492@gmail.com>
2018-06-29 20:21:21 -0500

    Add question validation for questions 1, 2, and 3

--- quiz.js
+++ quiz.js
@@ -1,26 +1,61 @@
 let button = document.querySelector('button');
-let labels = document.querySelectorAll('label');
+let questions = document.querySelectorAll('label');
 let questionOneAnswer = "anakin skywalker";
 let questionTwoAnswer = "revenge of the sith";
 let questionThreeAnswer = "luke skywalker";
 
-button.addEventListener("click", function(event) {
+function grade() {
   let questionOneChoices = document.querySelectorAll('input[name="answer1"]');
+  let questionTwoChoices = document.querySelectorAll('input[name="answer2"]');
+  let questionThreeChoices = document.querySelectorAll('input[name="answer3"]');
 
   for (let i = 0; i < questionOneChoices.length; i++) {
     if (questionOneChoices[i].checked) {
       let questionOneId = questionOneChoices[i].id;
-      labels.forEach(function(label) {
-        if(label.htmlFor == questionOneId) {
-          console.log(label.textContent.toLowerCase());
-          console.log(questionOneAnswer);
-          if(label.textContent.toLowerCase() == questionOneAnswer) {
-            console.log(label.textContent + ' is correct!');
+      questions.forEach(function(question) {
+        if(question.htmlFor == questionOneId) {
+          if(question.textContent.toLowerCase() == questionOneAnswer) {
+            console.log(question.textContent + ' is correct!');
+            question.innerHTML += " - Correct!";
           } else {
-            console.log(label.textContent + ' is incorrect!');
+            console.log(question.textContent + ' is incorrect!');
+            question.innerHTML += " - incorrect!";
           }
         }
       });
     }
   }
-});
+  for (let i = 0; i < questionTwoChoices.length; i++) {
+    if (questionTwoChoices[i].checked) {
+      let questionTwoId = questionTwoChoices[i].id;
+      questions.forEach(function(question) {
+        if(question.htmlFor == questionTwoId) {
+          if(question.textContent.toLowerCase() == questionTwoAnswer) {
+            console.log(question.textContent + ' is correct!');
+            question.innerHTML += " - Correct!";
+          } else {
+            console.log(question.textContent + ' is incorrect!');
+            question.innerHTML += " - Incorrect!";
+          }
+        }
+      });
+    }
+  }
+  for (let i = 0; i < questionThreeChoices.length; i++) {
+    if (questionThreeChoices[i].checked) {
+      let questionThreeId = questionThreeChoices[i].id;
+      questions.forEach(function(question) {
+        if(question.htmlFor == questionThreeId) {
+          if(question.textContent.toLowerCase() == questionThreeAnswer) {
+            console.log(question.textContent + ' is correct!');
+            question.innerHTML += " - Correct!";
+          } else {
+            console.log(question.textContent + ' is incorrect!');
+            question.innerHTML += " - Incorrect!";
+          }
+        }
+      });
+    }
+  }
+}
+button.addEventListener("click", grade);
