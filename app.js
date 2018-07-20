9b10fa28f3a383c19d929caf9206ff6a8b9729c5
defalt42 <bgilbert1492@gmail.com>
2018-07-20 15:14:58 -0500

    Create and add code to app.js, question.js, and quiz-controller.js

---
+++ app.js
@@ -0,0 +1,17 @@
+var questions = [
+  new Question("Who is Darth Vader?",
+  ["Luke Skywalker", "Anakin Skywalker", "Your Mom", "Your Dad"],
+  "Anakin Skywalker"),
+  new Question("What is the name of the third episode?",
+  ["Revenge of the Sith", "Return of the Jedi", "A New Hope", "Attack of the Clones"],
+  "Revenge of the Sith"),
+  new Question("Who is Anakin Skywalker's son?",
+  ["Luke Skywalker", "Anakin Skywalker", "Your Mom", "Your Dad"],
+  "Anakin Skywalker"),
+  new Question("What is the name of the sixth episode?",
+  ["Luke Skywalker", "Anakin Skywalker", "Your Mom", "Your Dad"],
+  "Anakin Skywalker"),
+];
+function populateQuestions() {
+
+}
